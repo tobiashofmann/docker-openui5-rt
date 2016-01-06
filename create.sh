f8c6e9ffec5ecfaa978981cbdbcafd92c2a72c2f
Tobias Hofmann <hofmann.tobias@outlook.com>
2016-01-06 10:35:32 -0200

    Create create.sh
    Shell script to build all tagged versions of openui5

---
+++ create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in $(git tag);
+do
+  echo $i;
+  git checkout $i
+  npm install
+  grunt build --production
+  mkdir -p /var/www/html/$i/resources
+  for f in ./target/*; do echo $f; cp $f/resources/* -R /var/www/html/$i/resources/; done
+  # clean up src and target
+  rm -R ./target/*
+  rmdir ./target
+done
